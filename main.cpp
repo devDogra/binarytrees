a4f61ac4b000a519a08f20f9eb9988223f91390d
Dev Dogra <48864825+devDogra@users.noreply.github.com>
2021-12-06 09:22:12 +0000

    Added zigzag traversal

--- main.cpp
+++ main.cpp
@@ -18,9 +18,32 @@ struct node
 std::vector<int> topview(node* root);
 
 void preorder(node* root);
-std::unordered_map<node*, int> levelorder(node* root, std::vector<int>& ans);
+std::vector<int> levelorder(node* root);
+
+node* build_tree()
+{
+	node* root = new node(1);
+
+	root->left = new node(2);
+	root->right = new node(3);
+
+	root->left->left = new node(4);
+	root->left->right = new node(5);
+	root->right->left = new node(6);
+	root->right->right = new node(7);
+
+	root->left->left->left = new node(8);
+	root->left->left->right = new node(9);
+	root->left->right->left = new node(10);
+	root->left->right->right = new node(11);
 
 
+	return root; 
+
+	
+
+
+}
 
 int main() 
 {
@@ -38,9 +61,16 @@ int main()
 
 	root->right->left->right->left->right = new node(5);
 
+
 	//preorder(root);
+	
+	root = build_tree();
+	root = new node(5);
+	root->left = new node(4);
+	root->right = new node(9);
+	root->left->left = new node(8);
 	std::vector<int> ans; // stores the level by level OT
-	ans  = topview(root);
+	ans  = levelorder(root);
 	//std::unordered_map<node*, int> nodewidth = levelorder(root, ans);
 
 	std::copy(ans.begin(), ans.end(), std::ostream_iterator<int>(std::cout, " "));
@@ -56,12 +86,12 @@ std::vector<int> topview(node* root)
 	std::deque<int> realans;
 	std::unordered_map<node*, int> map; // map from width->node*s
 	std::queue<node*> Q;
-	int cminw = 0; // curr min width seen
+	int cminw = 0;
 	int cmaxw = 0;
 	node* t = root;
 	std::map<int, node*> widths; // map from width to its 1st visited node
 	int w = 0;
-	/////////////////////////////////////////////////
+/////////////////////////////////////////////////
 	if (root == nullptr) return ans;
 
 	Q.push(t);
@@ -115,3 +145,86 @@ void preorder(node* root)
 	}
 }
 
+// prints only
+std::vector<int> levelorder_zigzag(node* root)
+{
+
+	std::vector<int> ans;
+	std::deque<int> LOS;
+	std::vector<node*> LON; // level order nodes
+
+	std::deque<node*> Q; // processing Q for the nodes of a level
+	std::deque<node*> S; // holding queue for the nodes of the next level
+
+	bool odd= true;
+
+	node* t = root;
+	S.push_back(root);
+	
+	if (root == nullptr) return ans;
+	
+	
+	while(!Q.empty() or !S.empty()){
+		
+		
+		if (Q.empty()) { // If processing is done for *this* level
+			// bring all of S into Q
+			Q = S;
+
+			// transfer this "level" into the vector
+			while(!S.empty())
+			{
+				// alternate b/w pushfront and pushback
+				// if (odd) LOS.push_back(S.front()->data); 
+				// else LOS.push_front(S.front()->data);
+
+				// odd
+				if (odd)
+				{
+					LOS.push_back(S.front()->data);	
+					S.pop_front();
+				}
+				else
+				{
+					LOS.push_back(S.back()->data);
+					S.pop_back();
+				}
+				// even 
+				
+
+			}
+
+			odd = !odd;
+			LON.push_back(nullptr); // to signal the end of the level
+			LOS.push_back(-1);
+
+			S = std::deque<node*>(); 
+
+			
+		} 
+
+		t = Q.front(); 
+		
+		
+		Q.pop_front();
+
+		if (t->left) { 
+			S.push_back(t->left); 
+		}
+		if (t->right) {
+			S.push_back(t->right);
+		}
+
+
+	}
+
+
+
+	for (auto it = LOS.begin(); it != LOS.end(); it++){
+		if (*it == -1) std::cout << "\n";
+		else std::cout << *it << "  ";
+	}
+
+	return ans;
+
+}
